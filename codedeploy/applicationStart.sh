bb2d8c296d21a548a35687011d1d0e410ce3b3eb
Rajasi Akolkar <akolkar.r@northeastern.edu>
2020-11-03 22:26:26 -0500

    changing codedeploy scripts

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/env bash
-echo 'Starting Spring Boot App'
-cd '/home/ubuntu/'
-java -jar ROOT.jar
+#!/bin/bash
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/*
+cd /home/ubuntu/myApp/
+
+sudo chmod +x ROOT.jar
+
+source /etc/profile.d/envvariable.sh
+
+nohup java -jar ROOT.jar > /home/ubuntu/applog.txt 2> /home/ubuntu/applog.txt < /home/ubuntu/applog.txt &
