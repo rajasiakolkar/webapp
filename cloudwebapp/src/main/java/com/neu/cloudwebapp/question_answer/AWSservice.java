6929cae440021b39ca11fe65f3b0c5f2b7c73649
Rajasi Akolkar <akolkar.r@northeastern.edu>
2020-12-02 03:35:57 -0500

    changes to AWS service

--- cloudwebapp/src/main/java/com/neu/cloudwebapp/question_answer/AWSservice.java
+++ cloudwebapp/src/main/java/com/neu/cloudwebapp/question_answer/AWSservice.java
@@ -7,13 +7,15 @@ import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AWSservice {
 
-    @Value("${aws.region}")
+    @Value("${aws.region:us-east-1}")
     String region;
 
-    @Value("${aws.profile}")
+    @Value("${aws.profile:#{null}}")
     String profile;
 
     @Bean
